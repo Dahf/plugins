6f9e6e24ef2138e2a69e983838b4252f00618cf0
dahf <flacezockt@gmail.com>
2020-11-16 18:28:04 +0100

    Login messages added

--- website/login/login.php
+++ website/login/login.php
@@ -7,44 +7,14 @@
     <link href="../style/login.css" rel="stylesheet">
   </head>
   <body>
-<!--login-->
-    <?php
-    if(isset($_POST["submit_login"])){
-      require("../mysql.php");
-      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
-      $stmt->bindParam(":user", $_POST["username_login"]);
-      $stmt->execute();
-      $count = $stmt->rowCount();
-      if($count == 1){
-        //Username ist frei
-        $row = $stmt->fetch();
-        if(password_verify($_POST["pw_login"], $row["PASSWORD"])){
-          session_start();
-          $_SESSION["username"] = $row["USERNAME"];
-          header("Location: dashboard.php");
-        } else {
-          echo "Der Login ist fehlgeschlagen";
-        }
-      } else {
-        echo "Der Login ist fehlgeschlagen";
-      }
-    }
-    ?>
-<!---------------- JAVASCRIPT ---------------->
-<<<<<<< HEAD
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"></script>
-<script>
-  AOS.init();
-  </script>
 
-=======
+<!---------------- JAVASCRIPT ---------------->
+
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"></script>
     <script>
       AOS.init();
       </script>
->>>>>>> 4cec7e9df445d07856f92aa1aede86fbe99d6fcb
 <!---------------- HEADER ---------------->
     <div id="header">
       <a class="headerwri"href="../index.php">
@@ -53,6 +23,7 @@
         </div>
       </a>
     </div>
+
 <!---------------- FOOTER ---------------->
     <div id="footer">
       <div id="links_footer">
@@ -66,7 +37,6 @@
       </div>
     </div>
 <!---------------- NAVBAR ---------------->
-<<<<<<< HEAD
         <div id="navbar">
              <div id="links_navbar">
                  <a class="navlink" href="spigot.html"><b>SPIGOT</b></a>
@@ -79,11 +49,9 @@
                  <?php endif; ?>
                </div>
          </div>
-<!---------------- COMMENTARY ---------------->
-<div class="commentary">
-  <p name="sd" id="commentary">sdf</p>
-</div>
+
 <!---------------- LOGIN ---------------->
+
 <div id="login">
   <div class="login-picture">
     <i class="fas fa-sign-in-alt fa-3x"></i>
@@ -94,92 +62,8 @@
     <input type="password" name="pw_login" placeholder="Passwort" required class="typein"><br>
     <button type="submit" name="submit_login" class="loggin-btn">Einloggen</button>
   </form>
-=======
-    <div id="navbar">
-      <div id="links_navbar">
-        <a class="navlink" href="spigot.html"><b>SPIGOT</b></a>
-        <a class="navlink" href="bungeecord.html"><b>BUNGEECORD</b></a>
-        <?php if (!isset($_SESSION['username'])): ?>
-        <a class="navlink" href="login/login.php"><b>LOGIN</b></a>
-        <?php endif; ?>
-        <?php if (isset($_SESSION['username'])): ?>
-        <a class="navlink" href="login/dashboard.php"><b>ACCOUNT</b></a>
-        <?php endif; ?>
-      </div>
-    <div class="shopping-cart">
-      <a class="shopping-btn" href="shoppingcart.php">
-        <i class="fas fa-shopping-cart"></i>
-      </a>
-    </div>
-    <div class="search-box">
-      <input type="text" name="" class="search-txt" placeholder="Type..."/>
-      <a class="search-btn">
-        <i class="fas fa-search"></i>
-      </a>
-    </div>
-  </div>
-<!---------------- LOGIN ---------------->
-  <div id="login">
-    <h1>Anmelden</h1>
-      <form action="login.php" method="post">
-      <input type="text" name="username_login" placeholder="Username" required class="typein"><br>
-      <input type="password" name="pw_login" placeholder="Passwort" required class="typein"><br>
-      <button type="submit" name="submit_login" class="login-btn">Einloggen</button>
-    </form>
->>>>>>> 4cec7e9df445d07856f92aa1aede86fbe99d6fcb
 </div>
 <!--register-->
-<?php
-if(isset($_POST["submit_register"])){
-  require("../mysql.php");
-  $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
-  $stmt->bindParam(":user", $_POST["username_register"]);
-  $stmt->execute();
-  $count = $stmt->rowCount();
-  if($count == 0){
-    //Username ist frei
-    $stmt = $mysql->prepare("SELECT * FROM accounts WHERE EMAIL = :email"); //Username überprüfen
-    $stmt->bindParam(":email", $_POST["email_register"]);
-    $stmt->execute();
-    $count = $stmt->rowCount();
-    if($count == 0){
-      if($_POST["pw_register"] == $_POST["pw_register2"]){
-        //User anlegen
-        $stmt = $mysql->prepare("INSERT INTO accounts (id, USERNAME, PASSWORD, EMAIL, TOKEN, SERVERRANK) VALUES (0, :user, :pw, :email, null, 0)");
-        $stmt->bindParam(":user", $_POST["username_register"]);
-        $hash = password_hash($_POST["pw_register"], PASSWORD_BCRYPT);
-        $stmt->bindParam(":pw", $hash);
-        $stmt->bindParam(":email", $_POST["email_register"]);
-        $stmt->execute();
-        ?>
-        <script>
-            document.getElementById("commentary").value = "Dein Account wurde angelegt";
-        </script>
-     <?php
-      } else {
-         ?>
-           <script>
-        document.getElementById("commentary").value = "Die Passwörter stimmen nicht überein";
-        </script>
-         <?php
-      }
-    } else {
-      ?>
-        <script>
-      document.getElementById("commentary").value = "Email bereits vergeben";
-      </script>
-       <?php
-    }
-  } else {
-    ?>
-    <script>
-          document.getElementById("commentary").value = "Der Username ist bereits vergeben";
-    </script>
-
-     <?php
-  }
-}
-?>
 <div id="register">
   <div class="register-picture">
     <i class="fas fa-user-plus fa-3x"></i>
@@ -192,6 +76,96 @@ if(isset($_POST["submit_register"])){
     <input type="password" name="pw_register2" placeholder="Passwort wiederholen" required class="typein"><br>
     <button type="submit" name="submit_register" class="loggin-btn">Erstellen</button>
   </form>
+</div>
+<div id="register">
+  <?php
+  if(isset($_POST["submit_register"])){
+    require("../mysql.php");
+    $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+    $stmt->bindParam(":user", $_POST["username_register"]);
+    $stmt->execute();
+    $count = $stmt->rowCount();
+    if($count == 0){
+      //Username ist frei
+      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE EMAIL = :email"); //Username überprüfen
+      $stmt->bindParam(":email", $_POST["email_register"]);
+      $stmt->execute();
+      $count = $stmt->rowCount();
+      if($count == 0){
+        if($_POST["pw_register"] == $_POST["pw_register2"]){
+          //User anlegen
+          $stmt = $mysql->prepare("INSERT INTO accounts (id, USERNAME, PASSWORD, EMAIL, TOKEN, SERVERRANK) VALUES (0, :user, :pw, :email, null, 0)");
+          $stmt->bindParam(":user", $_POST["username_register"]);
+          $hash = password_hash($_POST["pw_register"], PASSWORD_BCRYPT);
+          $stmt->bindParam(":pw", $hash);
+          $stmt->bindParam(":email", $_POST["email_register"]);
+          $stmt->execute();
+          echo '<p id="commentaryregister"> Kundenkonto angelegt</p>';
+          ?>
+          <script>
+              setTimeout(function(){document.getElementById('commentaryregister').remove();},10000);
+          </script>
+       <?php
+        } else {
+          echo '<p id="commentaryregister">Passwörter stimmen nicht übereinander ein</p>';
+          ?>
+          <script>
+              setTimeout(function(){document.getElementById('commentaryregister').remove();},10000);
+          </script>
+           <?php
+        }
+      } else {
+        echo '<p id="commentaryregister">E-Mail bereits vergeben</p>';
+        ?>
+        <script>
+            setTimeout(function(){document.getElementById('commentaryregister').remove();},10000);
+        </script>
+         <?php
+      }
+    } else {
+      echo '<p id="commentaryregister">Username bereits vergeben </p>';
+      ?>
+      <script>
+          setTimeout(function(){document.getElementById('commentaryregister').remove();},10000);
+      </script>
+       <?php
+    }
+  }
+  ?>
+</div>
+<div id="login">
+  <?php
+  if(isset($_POST["submit_login"])){
+    require("../mysql.php");
+    $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+    $stmt->bindParam(":user", $_POST["username_login"]);
+    $stmt->execute();
+    $count = $stmt->rowCount();
+    if($count == 1){
+      //Username ist frei
+      $row = $stmt->fetch();
+      if(password_verify($_POST["pw_login"], $row["PASSWORD"])){
+        session_start();
+        $_SESSION["username"] = $row["USERNAME"];
+        header("Location: dashboard.php");
+      } else {
+        echo '<p id="commentarylogin"> Login fehlgeschlagen</p>';
+        ?>
+        <script>
+            setTimeout(function(){document.getElementById('commentarylogin').remove();},10000);
+        </script>
+      <?php
+      }
+    } else {
+        echo '<p id="commentarylogin"> Login fehlgeschlagen</p>';
+      ?>
+      <script type="text/javascript">
+            setTimeout(function(){document.getElementById('commentarylogin').remove();},10000);
+      </script>
+    <?php
+    }
+  }
+  ?>
 </div>
   </body>
 </html>
