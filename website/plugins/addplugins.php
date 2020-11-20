9824faf1e347c936208fb13eb6ca072f06ec31d5
dahf <flacezockt@gmail.com>
2020-11-20 16:11:28 +0100

    You can add pictures now

--- website/plugins/addplugins.php
+++ website/plugins/addplugins.php
@@ -18,19 +18,28 @@ if(isBanned($_SESSION["username"])){
     <?php
     if(isset($_POST["submit"])){
         require("../mysql.php");
-        $stmt = $mysql->prepare("INSERT INTO plugins (id, TITEL, DESCRIPTION, CREATED_AT) VALUES (0, :titel, :description, :now)");
+
+        $stmt = $mysql->prepare("INSERT INTO plugins (id, TITEL, DESCRIPTION, CREATED_BY, PICTURE) VALUES (0, :titel, :description, :by, :picture)");
+
         $stmt->bindParam(":titel", $_POST["titel"], PDO::PARAM_STR);
         $stmt->bindParam(":description", $_POST["description"], PDO::PARAM_STR);
-        $now = time();
-        $stmt->bindParam(":now", $now, PDO::PARAM_STR);
+        $file_name = $_FILES['file']['name'];
+        $file_tem_loc = $_FILES['file']['tmp_name'];
+        $file_store = "../upload/".$file_name;
+        $stmt->bindParam(":picture", $file_name, PDO::PARAM_STR);
+
+        $stmt->bindParam(":by", $_SESSION['username'], PDO::PARAM_STR);
         $stmt->execute();
+        if(move_uploaded_file($file_tem_loc, $file_store)){
         echo "Die News wurde erfolgreich hinzugefügt.";
+        }
 
     }
     ?>
-    <form action="addplugins.php" method="post">
+    <form action="?" method="post" enctype="multipart/form-data">
         <input type="text" name="titel" placeholder="Titel" required><br>
         <textarea name="description" cols="30" rows="10"></textarea><br>
+        <input type="file" name="file"><br>
         <button type="submit" name="submit">Hinzufügen</button><br>
     </form>
 </body>
