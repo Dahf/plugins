3bc63a434b5d2ddf1147b3ffbc31272bc00eb8fb
dahf <flacezockt@gmail.com>
2020-11-20 16:11:02 +0100

    Updated

--- website/news.php
+++ website/news.php
@@ -5,28 +5,38 @@
     </head>
     <body>
         <div class="separator">MOST POPULAR</div>
+          <div id="projects">
   <?php
    require("mysql.php");
-   $stmt = $mysql->prepare("SELECT * FROM plugins ORDER BY CREATED_AT DESC LIMIT 3");
+   $stmt = $mysql->prepare("SELECT * FROM plugins ORDER BY id DESC LIMIT 9");
    $stmt->execute();
    $count = $stmt->rowCount();
    if($count == 0){
        echo "Es wurden keine News gefunden.";
    } else {
+     ?>
+        <ul>
+     <?php
        while($row = $stmt->fetch()){
            ?>
-           <ul>
+
                <li>
-                   <b id="header"><?php echo $row["TITEL"] ?></b>
-                   <p id="status"><?php echo date("d.m.Y H:i", $row["CREATED_AT"]) ?></p>
+                   <img id="picture" src="upload/<?php echo $row["PICTURE"]?>">
+                   <div id="header">
+                     <a href="#"><?php echo $row["TITEL"] ?></a>
+                  </div>
+                   <p id="status"><?php echo $row["CREATED_BY"] ?></p>
                    <p><?php echo ($row["DESCRIPTION"]) ?></p>
-                   </li>
-                </ul>
+               </li>
+
 
            <?php
        }
+       ?>
+       </ul>
+       <?php
    }
    ?>
-
+      </div>
     </body>
 </html>
