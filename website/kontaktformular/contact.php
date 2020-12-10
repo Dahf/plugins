93b35225b4d05b4fbccdc49a7d5c527e96cb20e0
dahf <flacezockt@gmail.com>
2020-12-10 13:25:38 +0100

    Design Updates

--- website/kontaktformular/contact.php
+++ website/kontaktformular/contact.php
@@ -1,22 +1,16 @@
 <?php
 // Die Absender E-Mail Adresse
-$from = 'Citroen H Fan <hagen@novatrend.ch>';
+$from = 'Nutzer <test@gmail.com>';
 // Die Emfänger E-Mail Adresse
-$sendTo = 'Silas <silasbeckmann1508@gmail.com>';
-// E-Mail Betreff
-$subject = 'Eine neue Nachricht von einem Citroen H Fan';
-// Feldernamen und ihre Übersetzungen
-// Ein Array bestehend aus dem Variablennamen => Tect der in der Email erscheint
+$sendTo = 'Silas Beckmann <silasbeckmann1508@gmail.com>';
+$subject = 'Kontaktformular';
 $fields = array('name' => 'Name', 'email' => 'E-Mail', 'message' => 'Nachricht');
-// Nachricht, wenn die E-Mail verschicht wurde
-$okMessage = 'Deine Nachricht wurde verschickt. Vielen Dank. Wenn du deine E-Mail angegeben hast, melde ich mich vielleicht bei dir!';
-// Nachricht, wenn die E-Mail nicht verschicht wurde
+$okMessage = 'Deine Nachricht wurde verschickt. Vielen Dank.';
 $errorMessage = 'Irgendwas ging schief bei der Übermittlung der Daten. Versuche es einfach später nochmal.';
 try{
   if(count($_POST) == 0) throw new \Exception('Formular ist leer');
-  $emailText = "Neue Nachricht aus dem Citroen H Formular \n=============================\n";
+  $emailText = "Neue Nachricht \n__________________________\n";
   foreach ($_POST as $key => $value) {
-    // Wenn ein Feld da ist, schreibe es in die E-Mail
     if (isset($fields[$key])) {
       $emailText .= "$fields[$key]: $value\n";
     }
@@ -28,6 +22,7 @@ try{
     'Return-Path: ' . $from,
     );
   header("Location: ../index.php");
+  //email -> SMTP Server aufsetzen
   $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
 catch (\Exception $e){
